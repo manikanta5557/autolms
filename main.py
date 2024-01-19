ec59d807ea7c204a161d09c34729425243ce7e10
Manikanta Dommati <75038306+manikanta5557@users.noreply.github.com>
2024-01-19 20:15:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -119,7 +119,7 @@ def linkopener3():
 			if isNowInTimePeriod('8:00','8:55',timeNow):
 				startBot('9:00:01',usrname,passw,ioe412)
 				flag=1
-			if isNowInTimePeriod('07:00','24:55',timeNow):
+			if isNowInTimePeriod('07:00','23:55',timeNow):
 				print("#############   ics423 ######################")
 				startBot(':',usrname,passw,ics423)
 				flag=1
